dadd6026bbcfdcc70a05407cd2149bb2f0fe62c2
Matt Denwood <md@sund.ku.dk>
2026-08-24 19:18:36 +0200

    New jags-0 script

--- utils/jags-0.sh
+++ utils/jags-0.sh
@@ -10,31 +10,48 @@
 set -euo pipefail
 
 # Return codes:
-EX_OK=0
-EX_USAGE=64
-EX_CONFIG=78
+local EX_OK=0
+local EX_USAGE=64
+local EX_CONFIG=78
 
 ## Hard-code:
-MAJ="__MAJ__"
+local MAJ="__MAJ__"
 MAJ=5
 
 # Default build:
-BUILD="current-$MAJ"
+local BUILD="current-$MAJ"
 
-# Filtered arguments to pass directly to JAGS:
-local -a JAGSARGS
+# Possible script argument:
+local SCRIPT=""
 
-# Loop through all passed arguments
 for arg in "$@"; do
   # Extract possible build:
-  if [[ "$arg" == -b=* ]]; then
-    BUILD="${arg#*-b=}"
+  if [[ "$arg" == -b=* ]] || [[ "$arg" == --build=* ]]; then
+    if [[ "$BUILD" == "current-$MAJ" ]]; then
+      BUILD="${arg#*=}"
+    else
+      echo "Error: multiple -b and --build options detected" >&2
+      exit $EX_USAGE
+    fi
+    continue
+  elif [[ "$arg" == -d* ]] || [[ "$arg" == --debugger* ]]; then
+    echo "Debugger options ($arg) are not supported by jags-$MAJ - use jags directly" >&2
+    exit $EX_USAGE    
+  elif [[ "$arg" == -* || "$arg" == --* ]]; then
+    echo "Unsupported option: $arg" >&2
+    exit $EX_USAGE
+  else
+    if [[ "$SCRIPT" == "" ]]; then
+      SCRIPT="$arg"
+    else
+      echo "Error: multiple arguments detected - only --build option and a\nsingle (scriptfile) argument are supported" >&2
+      exit $EX_USAGE
+    fi
     continue
   fi
-  JAGSARGS+=("$arg")
 done
 
-## Verify the specified build exists:
+## Verify that the specified build exists:
 if [[ ! -d "/opt/jags/versions/jags/$BUILD" ]]; then
   echo "Error: no JAGS build at /opt/jags/versions/jags/$BUILD detected!\nUse jags-version -l to display available builds." 1>&2
   exit $EX_USAGE  
@@ -43,7 +60,27 @@ majvers=$(cat "/opt/jags/versions/jags/$BUILD/include/JAGS/version.h" | grep "JA
 if [[ ! "$majvers" == "$MAJ" ]]; then
   echo "Error: the JAGS build specified has a major version number of $majvers not $MAJ" 1>&2
   exit $EX_USAGE  
-fi  
+fi 
+
+## Set prefix:
+local PREFIX="/opt/jags/versions/jags/$BUILD"
 
 # Launch the specified build with arguments passed through:
 /opt/jags/versions/jags/$BUILD/bin/jags "${JAGSARGS[@]}"
+
+## Set environmental variables:
+if test -z "${LD_LIBRARY_PATH}"; then
+   export LD_LIBRARY_PATH="${PREFIX}/lib"
+else
+   export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
+fi
+export LTDL_LIBRARY_PATH="${JAGS_LIBS}:${PREFIX}/lib/JAGS/modules-${MAJ}"
+
+## Startup with or without script:
+if [[ "$SCRIPT" == "" ]]; then
+  echo "Startup JAGS $BUILD with no script"
+  exec "${PREFIX}/bin/jags"
+else
+  echo "Startup JAGS $BUILD with scriptfile $SCRIPT"
+  exec "${PREFIX}/bin/jags" "${SCRIPT}"
+fi
